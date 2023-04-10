1ac7d063cf86778228e4c2102cba3995dddd9821
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-10 17:47:31 +0200

    linter

--- java.js
+++ java.js
@@ -1,9 +1,9 @@
-const myopen = document.querySelector(".open");
-const nav = document.querySelector(".nav-links-2");
-const myaxe = document.querySelector(".axe");
-const mytra1 = document.querySelector(".nav-transition-1");
-const mytra2 = document.querySelector(".nav-transition-2");
-const mytra3 = document.querySelector(".nav-transition-3");
+let myopen = document.querySelector("open");
+let nav = document.querySelector(".nav-links-2");
+let myaxe = document.querySelector(".axe");
+let mytra1 = document.querySelector(".nav-transition-1");
+let mytra2 = document.querySelector(".nav-transition-2");
+let mytra3 = document.querySelector(".nav-transition-3");
 myopen.onclick = function () {
   nav.style.cssText = "transform: translateY(0%);";
 };
@@ -11,11 +11,5 @@ myaxe.onclick = function () {
   nav.style.cssText = "transform: translateY(-100%);";
 };
 mytra1.onclick = function () {
-  nav.style.cssText = "transform: translateY(-100%);";
-};
-mytra2.onclick = function () {
-  nav.style.cssText = "transform: translateY(-100%);";
-};
-mytra3.onclick = function () {
   nav.style.cssText = "transform: translateY(-100%);";
 };
