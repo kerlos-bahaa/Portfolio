6e58d88da67c69118e32b00d8b12741d6054eb2c
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-10 18:21:46 +0200

    eslint fix

--- java.js
+++ java.js
@@ -1,15 +1,14 @@
-let myopen = document.querySelector("open");
-let nav = document.querySelector(".nav-links-2");
-let myaxe = document.querySelector(".axe");
-let mytra1 = document.querySelector(".nav-transition-1");
-let mytra2 = document.querySelector(".nav-transition-2");
-let mytra3 = document.querySelector(".nav-transition-3");
+const myopen = document.querySelector('open');
+const nav = document.querySelector('.nav-links-2');
+const myaxe = document.querySelector('.axe');
+const mytra1 = document.querySelector('.nav-transition-1');
+
 myopen.onclick = function () {
-  nav.style.cssText = "transform: translateY(0%);";
+  nav.style.cssText = 'transform: translateY(0%);';
 };
 myaxe.onclick = function () {
-  nav.style.cssText = "transform: translateY(-100%);";
+  nav.style.cssText = 'transform: translateY(-100%);';
 };
 mytra1.onclick = function () {
-  nav.style.cssText = "transform: translateY(-100%);";
+  nav.style.cssText = 'transform: translateY(-100%);';
 };
