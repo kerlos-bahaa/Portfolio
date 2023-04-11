099e5e3879f993d8d03861a6440ff7839bcf263f
kerlosbahaa3 <aboelking4@gmail.com>
2023-04-11 05:09:24 +0200

    Fix

---
+++ java-1.js
@@ -0,0 +1,22 @@
+let myopen = document.querySelector(".open");
+let nav = document.querySelector(".nav-links-2");
+let myaxe = document.querySelector(".axe");
+let mytra1 = document.querySelector(".nav-transition-1");
+let mytra2 = document.querySelector(".nav-transition-2");
+let mytra3 = document.querySelector(".nav-transition-3");
+
+myopen.onclick = function () {
+  nav.style.cssText = "transform:translateY(0%);";
+};
+myaxe.onclick = function () {
+  nav.style.cssText = "transform: translateY(-100%);";
+};
+mytra1.onclick = function () {
+  nav.style.cssText = "transform: translateY(-100%);";
+};
+mytra2.onclick = function () {
+  nav.style.cssText = "transform: translateY(-100%);";
+};
+mytra3.onclick = function () {
+  nav.style.cssText = "transform: translateY(-100%);";
+};
